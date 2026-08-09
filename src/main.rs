e1db514cc66a73540a429bb30f8590e5f30c0137
Faizeen Hoque <haquefaizeen11@gmail.com>
2026-08-10 03:02:36 +0600

    refactor: move App struct implementation to a new app.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod app;
+
 use std::sync::Arc;
 
 #[cfg(target_arch = "wasm32")]
@@ -19,24 +21,6 @@ impl State {
 
     pub fn render(&mut self) {
         self.window.request_redraw();
-    }
-}
-
-pub struct App {
-    #[cfg(target_arch = "wasm32")]
-    proxy: Option<winit::event_loop::EventLoopProxy<State>>,
-    state: Option<State>,
-}
-
-impl App {
-    pub fn new(#[cfg(target_arch = "wasm32")] event_loop: &EventLoop<State>) -> Self {
-        #[cfg(target_arch = "wasm32")]
-        let proxy = Some(event_loop.create_proxy());
-        Self {
-            state: None,
-            #[cfg(target_arch = "wasm32")]
-            proxy,
-        }
     }
 }
 
